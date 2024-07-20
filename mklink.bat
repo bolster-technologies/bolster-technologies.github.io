fd0b2040c45b6aef70800137acddca595a7e34a8
Strata Haywire <strata.haywire@gmail.com>
2024-07-20 11:15:44 +0900

    vket2024s

---
+++ mklink.bat
@@ -0,0 +1,2 @@
+mklink "E:/Project/Unity/Tripwire/Web/tripwire.github.io/data/Characters.json" "E:/Project/Unity/Tripwire/Data/Characters.json"
+mklink /d "E:/Project/Unity/Tripwire/Web/tripwire.github.io/images/character" "E:/Project/Unity/Tripwire/Data/Images/Character"
