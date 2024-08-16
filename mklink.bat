36014d15dff8b82db1d2c54c0615b7fe76dcf1c7
Strata Haywire <strata.haywire@gmail.com>
2024-08-16 20:09:37 +0900

    overall update

---
+++ mklink.bat
@@ -0,0 +1,3 @@
+mklink /h "E:/Project/Unity/TripwireRacing/Web/bolster-technologies.github.io/data/Characters.json" "E:/Project/Unity/TripwireRacing/Data/Characters.json"
+mklink /j "E:/Project/Unity/TripwireRacing/Web/bolster-technologies.github.io/images/character" "E:/Project/Unity/TripwireRacing/Data/Images/Character"
+pause
